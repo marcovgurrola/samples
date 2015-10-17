71d11d1cdafe018b8bf4ce93702537593c1705e9
marco <marco@marcos-MacBook-Pro.local>
2015-10-17 10:16:31 -0300

    9-1 Basic error reporting

---
+++ applications/MeteorPost/microscope/client/helpers/errors.js
@@ -0,0 +1,12 @@
+// local (client-only) collection
+Errors = new Mongo.Collection(null);
+
+throwError = function(message) {
+	Errors.insert({message: message});
+};
+
+Template.errors.helpers({
+	errors: function() {
+		return Errors.find();
+	}
+});
