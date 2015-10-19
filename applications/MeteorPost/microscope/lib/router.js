130e2f871d8d37327a503ef0257a1e2f279d728c
marco <marco@marcos-MacBook-Pro.local>
2015-10-19 17:11:41 -0200

    12-3 Refactored postsList route into a RouteController

--- applications/MeteorPost/microscope/lib/router.js
+++ applications/MeteorPost/microscope/lib/router.js
@@ -3,15 +3,33 @@ Router.configure({
   loadingTemplate: 'loading',
   notFoundTemplate: 'notFound',
   waitOn: function() { 
-    return [Meteor.subscribe('notifications')];
+    return [Meteor.subscribe('notifications')]
   }
 });
 
+PostsListController = RouteController.extend({
+  template: 'postsList',
+  increment: 5, 
+  postsLimit: function() { 
+    return parseInt(this.params.postsLimit) || this.increment; 
+  },
+  findOptions: function() {
+    return {sort: {submitted: -1}, limit: this.postsLimit()};
+  },
+  waitOn: function() {
+    return Meteor.subscribe('posts', this.findOptions());
+  },
+  data: function() {
+    return {posts: Posts.find({}, this.findOptions())};
+  }
+});
+
+
 Router.route('/posts/:_id', {
   name: 'postPage',
-	waitOn: function() {
-		return Meteor.subscribe('comments', this.params._id);
-	},
+  waitOn: function() {
+    return Meteor.subscribe('comments', this.params._id);
+  },
   data: function() { return Posts.findOne(this.params._id); }
 });
 
@@ -22,23 +40,11 @@ Router.route('/posts/:_id/edit', {
 
 Router.route('/submit', {name: 'postSubmit'});
 
-Router.route('/:postsLimit?', {
-	name: 'postsList',
-	waitOn: function() {
-		var limit = parseInt(this.params.postsLimit) || 5;
-		return Meteor.subscribe('posts', {sort: {submitted: -1}, limit: limit});
-	},
-	data: function() {
-		var limit = parseInt(this.params.postsLimit) || 5;
-		return {
-			posts: Posts.find({}, {sort: {submitted: -1}, limit: limit})
-		};
-	}
-});
+Router.route('/:postsLimit?', {name: 'postsList'});
 
 var requireLogin = function() {
-  if(! Meteor.user()) {
-    if(Meteor.loggingIn()) {
+  if (! Meteor.user()) {
+    if (Meteor.loggingIn()) {
       this.render(this.loadingTemplate);
     } else {
       this.render('accessDenied');
